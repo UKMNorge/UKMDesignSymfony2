6ca6075274620677af0465bc53513b0f0129de29
Asgeir Stavik Hustad <askhus@online.no>
2020-02-17 23:52:00 +0100

    UKMDesign-service

---
+++ DesignBundle/Services/UKMDesign.php
@@ -0,0 +1,56 @@
+<?php
+namespace UKMNorge\DesignBundle\Services;
+
+use UKMNorge\Design\Sitemap\Section;
+use UKMNorge\Design\Image;
+use UKMNorge\Design\UKMDesign as UKMNorgeUKMDesign;
+
+class UKMDesign extends UKMNorgeUKMDesign {
+    public function __construct()
+    {
+        require_once('UKMconfig.inc.php');
+        static::setCurrentSection(
+            new Section(
+                'delta',
+                'delta.'. UKM_HOSTNAME,
+                'Delta på UKM'
+            )
+        );
+
+        static::_initUKMDesign();
+
+
+
+    }
+
+    /**
+     * Sett opp standard-data i UKMDesign
+     * 
+     * Basert på konfig - setter standard-data for SEO blant annet
+     *
+     * @return void
+     */
+    private static function _initUKMDesign()
+    {
+        UKMDesign::init();
+        UKMDesign::getHeader()::getSeo()
+            ->setImage(
+                new Image(
+                    UKMDesign::getConfig('SEOdefaults.image.url'),
+                    intval(UKMDesign::getConfig('SEOdefaults.image.width')),
+                    intval(UKMDesign::getConfig('SEOdefaults.image.height')),
+                    UKMDesign::getConfig('SEOdefaults.image.type')
+                )
+            )
+            ->setSiteName(UKMDesign::getConfig('SEOdefaults.site_name'))
+            ->setType('website')
+            ->setTitle(UKMDesign::getConfig('SEOdefaults.title'))
+            ->setDescription(UKMDesign::getConfig('slogan'))
+            ->setAuthor(UKMDesign::getConfig('SEOdefaults.author'))
+            ->setFBAdmins(UKMDesign::getConfig('facebook.admins'))
+            ->setFBAppId(UKMDesign::getConfig('facebook.app_id'))
+            ->setGoogleSiteVerification(UKMDesign::getConfig('google.site_verification'));
+    }
+
+
+}
